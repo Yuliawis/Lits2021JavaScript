c4bc00fac726b22e33fc326ff33d59af9082470c
Yuliawis <95494517+Yuliawis@users.noreply.github.com>
2022-01-03 16:52:17 +0200

    Add files via upload

---
+++ homework3/script.js
@@ -0,0 +1,16 @@
+console.log("Task 1");
+
+function number(num1, num2, num3){ 
+       console.log(Math.max(num1, num2, num3)) 
+    } 
+number(5, 6, 1);
+
+console.log("Task 2")
+
+// let timerId = setInterval(() => document.write("<p>Привіт</p>"), 2000);
+
+
+console.log("Task 3")
+
+
+let timerId = setTimeout(() => alert("Ти ще тут?"), 3000);
